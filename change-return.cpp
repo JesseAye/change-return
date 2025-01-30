30dd4e933f527790896131831b5a6c6f30956760
Jesse Ayala <jesserayala95@gmail.com>
2025-01-30 01:32:07 -0500

    New struct for the change return transaction Logic of calculating the change now in separate function

--- change-return.cpp
+++ change-return.cpp
@@ -3,40 +3,53 @@
 #include <map>
 #include <string>
 
-int main(int argc, char *argv[])
-{
-    std::map<uint, uint> change_to_return;
-    std::map<uint, std::string> denominations_cents = {
-        {10000, "$100"},
-        { 5000, "$50"},
-        { 2000, "$20"},
-        { 1000, "$10"},
-        {  500, "$5"},
-        {  100, "$1"},
-        {   25, "$0.25"},
-        {   10, "$0.10"},
-        {    5, "$0.05"},
-        {    1, "$0.01"}
+struct change {
+    uint total_change;
+    std::map<uint, uint> change_to_return = {
+        {10000, 0},
+        { 5000, 0},
+        { 2000, 0},
+        { 1000, 0},
+        {  500, 0},
+        {  100, 0},
+        {   25, 0},
+        {   10, 0},
+        {    5, 0},
+        {    1, 0}
     };
-    float order_total = 102.34f, tendered = 200.00f; // User-provided data, just test values for now
+};
+
+change calculate_change(float order_total, float tendered)
+{
+    change result;
     uint order_total_cents = ((uint)(order_total * 100)),
         tendered_cents = ((uint)(tendered * 100));
-    float remaining_change = tendered - order_total;
-    uint remaining_change_cent = tendered_cents - order_total_cents;
-
-    printf("Calculating denominations to return $%.2f:\n", remaining_change);
-
-    for (std::map<uint, std::string>::reverse_iterator rit = denominations_cents.rbegin(); rit != denominations_cents.rend(); ++rit)
+    uint remaining_change_cent = result.total_change = tendered_cents - order_total_cents;
+    
+    for (std::map<uint, uint>::reverse_iterator rit = result.change_to_return.rbegin(); rit != result.change_to_return.rend(); ++rit)
     {
         if(remaining_change_cent >= rit->first)
         {
             do
             {
                 remaining_change_cent -= rit->first;
-                change_to_return[rit->first]++;
+                rit->second++;
             } while (remaining_change_cent >= rit->first);
-            printf("%5s - %lu\n", rit->second.c_str(), change_to_return[rit->first]);
         }
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    float order_total = 102.34f, tendered = 200.00f; // User-provided data, just test values for now
+
+    change order_return = calculate_change(order_total, tendered);
+    printf("Total to return: $%.2f\n", (((float)order_return.total_change)/100.00f));
+    for (std::map<uint, uint>::reverse_iterator rit = order_return.change_to_return.rbegin(); rit != order_return.change_to_return.rend(); ++rit)
+    {
+        printf("$%6.2f - %lu\n", (((float)rit->first)/100.00f), rit->second);
     }
 
     return 0;
